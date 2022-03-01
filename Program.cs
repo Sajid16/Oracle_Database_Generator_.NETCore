49c3777b746f35ceb041debfa07dafae2586367c
sajid16 <sourav@erainfotechbd.com>
2022-03-01 13:21:13 +0600

    initial commit with generation of entities tested successfully from oracle

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oracle_Database_Generator_.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
